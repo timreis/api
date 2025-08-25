f173012f23dec27e7864fbe315974f8a119a7801
Tiago Martins Reis <127058546+timreis@users.noreply.github.com>
2025-08-25 09:06:39 -0300

    Create assun.js

---
+++ assun.js
@@ -0,0 +1,15 @@
+{
+    "Flamengo": 24.8,
+    "Corinthians": 19.4,
+    "Palmeiras": 8.1,
+    "São Paulo": 7.7,
+    "Vasco": 4.8,
+    "Grêmio": 4.4,
+    "Atlético-MG": 4.0,
+    "Santos": 3.1,
+    "Internacional": 2.9,
+    "Cruzeiro": 2.7     
+}
+
+
+  

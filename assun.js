28d3cf5896a7318cd47cf0e52e4e98ea1b09acc6
Tiago Martins Reis <127058546+timreis@users.noreply.github.com>
2025-08-28 18:18:52 -0300

    atualizado

--- assun.js
+++ assun.js
@@ -2,10 +2,10 @@
     "Flamengo": 24.8,
     "Corinthians": 19.4,
     "Palmeiras": 8.1,
-    "São Paulo": 7.7,
+    "Sao_Paulo": 7.7,
     "Vasco": 4.8,
-    "Grêmio": 4.4,
-    "Atlético-MG": 4.0,
+    "Gremio": 4.4,
+    "Atletico_MG": 4.0,
     "Santos": 3.1,
     "Internacional": 2.9,
     "Cruzeiro": 2.7     
